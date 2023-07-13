987a8fbbf2f6e10f2a52b7a748d028c412b96465
wk00100 <wanderwalker0110@protonmail.com>
2023-07-13 08:30:12 +0800

    Use local certificate picture instead of using url

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,11 +2,11 @@
   <main class="about">
     <h1>關於月欣</h1>
     <div class="certificate">
-      <img alt="certificate" src="http://www.moonstar-meter.com/pic/ce.jpg" width="300" />
+      <img alt="certificate" src="@/assets/certificate.jpg" width="300" />
     </div>
     <article>
       <p>
-        &emsp;&emsp;月欣科技有限公司創立於民國89年，主要從事專業電子儀錶，如:計數器,長度表,電壓表,電流錶,線速表,轉速表,控制器,安培小時,光電近接,壓力流量．．．等
+        &emsp;&emsp;月欣科技有限公司創立於民國89年，主要從事專業電子儀錶，如:計數器，長度表，電壓表，電流錶，線速表，轉速表，控制器，安培小時，光電近接，壓力流量．．．等
         ；擁有為數不少的客戶群。<br />
         &emsp;&emsp;本公司擁有優秀的經營團隊，秉持著『專業技術』的經營理念，追求企業永續經營及成長；除整體營運穩定外，獲利狀況也逐年提升，是國內績優廠商之一
         。<br />
